ac80cf5d0809a29e169fb1d09dcec4e11b959649
Efrat <efratdr6179@gmail.com>
2025-11-06 11:32:12 +0200

    Enhance user management by adding email notifications for existing users and creating new users with temporary passwords

--- app/lib/mailer.ts
+++ app/lib/mailer.ts
@@ -2,10 +2,14 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
-export async function sendPasswordEmail(to: string, password: string, manger:string) {
+export async function sendPasswordEmail(
+  to: string,
+  password: string,
+  manger: string
+) {
   const msg = {
     to,
-    from: process.env.FROM_EMAIL!, 
+    from: process.env.FROM_EMAIL!,
     subject: "Your Temporary Password",
     html: `
       <div style="font-family:Arial;direction:rtl;">
@@ -17,6 +21,23 @@ export async function sendPasswordEmail(to: string, password: string, manger:str
         <p>When you first log in, you will be asked to update your details.</p>
       </div>
     `,
+  };
+
+  await sgMail.send(msg);
+}
+
+export async function sendExistMail(to: string, manger: string) {
+  const msg = {
+    to,
+    from: process.env.FROM_EMAIL!,
+    subject: "You Have Been Added to a New Project",
+    html: `
+     <div style="font-family: Arial, sans-serif;">
+  <p>Hello,</p>
+  <p>You’ve been added to a new project in the <strong>Account Management System</strong>.</p>
+  <p>You can now log in to view the project details and start collaborating with your team.</p>
+  <p>Best regards,<br/>The Project Management Team</p>
+</div>`,
   };
 
   await sgMail.send(msg);
