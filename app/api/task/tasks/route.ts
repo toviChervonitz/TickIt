bce8cbcd1ab2f5dd4e8760075f00e27ce9ab1881
toviChervonitz <tovichervonitz1234@gmail.com>
2025-11-09 11:46:30 +0200

    add get all task by userId add component to show a one task

---
+++ app/api/task/tasks/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { dbConnect } from "@/app/lib/DB";
+import Task from "@/app/models/TaskModel";
+
+export async function GET(req: Request) {
+    await dbConnect();
+
+
+    try {
+        
+        const authHeader = req.headers.get("authorization");
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get("userId");
+
+        if (!userId) {
+            return NextResponse.json(
+                { status: "error", message: "userId is required" },
+                { status: 400 }
+            );
+        }
+        const tasks = await Task.find({ userId })
+            .populate("userId", "name")
+            .populate("projectId", "name");
+
+        if (!tasks || tasks.length === 0) {
+            return NextResponse.json(
+                { status: "success", message: "No tasks found", tasks: [] },
+                { status: 200 }
+            );
+        }
+        return NextResponse.json(
+            {
+                status: "success",
+                message: "Tasks fetched successfully",
+                count: tasks.length,
+                tasks,
+            },
+            { status: 200 }
+        );
+    } catch (err: any) {
+        console.error("Get Tasks Error:", err);
+        return NextResponse.json(
+            { status: "error", message: "Server error" },
+            { status: 500 }
+        );
+    }
+}
+
