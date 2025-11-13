685c3a506cde07c6a45a93d7ae65993608108cbb
Efrat <efratdr6179@gmail.com>
2025-11-13 12:47:28 +0200

    Refactor user management functions for improved readability and consistency; add API endpoint to fetch all users in a project.

--- app/pages/projectTask/page.tsx
+++ app/pages/projectTask/page.tsx
@@ -11,6 +11,8 @@ import { getUserRoleInProject } from "@/app/lib/server/projectServer";
 import Task from "@/app/components/Task";
 import { IProject, ITask, IUser } from "@/app/models/types";
 import { set } from "mongoose";
+import { getAllUsersByProjectId } from "@/app/lib/server/userServer";
+import { useRouter } from "next/navigation";
 
 interface ProjectType {
   _id: string;
@@ -23,59 +25,61 @@ interface ProjectType {
 }
 
 export default function GetProjectTasks() {
-  const { projectId, tasks, setTasks, user } = useAppStore();
+  const { projectId, tasks, setTasks, user, setProjectUsers } = useAppStore();
   const [filteredTasks, setFilteredTasks] = useState<ITask[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [isManager, setIsManager] = useState(false);
- async function loadTasks() {
-      try {
-        if (!projectId && !user) {
-          setLoading(false);
-          return;
-        }
-        let data;
-        const userId = user?._id;
-        //check is manger
-        const role = await getUserRoleInProject(userId, projectId);
-        console.log("get to this?1", role);
-
-        if (role !== "viewer") {
-          setIsManager(true);
-          //get from db all tasks by projects
-          data = await GetTasksByProjectId(userId!, projectId);
-          console.log("get to this?2");
-          console.log("GetTasksByProjectId", data);
-          setFilteredTasks(data);
-        } else {
-          //:if !tasks
-          if (!tasks || tasks.length === 0) {
-            //get all tasks by userId and put it in store
-
-            data = await GetTasksByUserId(user?._id);
-            console.log("get to this?3");
-            console.log("GetTasksByUserId", data);
-            setTasks(data);
-          }
-          //filter tasks by projectId
-          const filtered = tasks.filter(
-            (task: any) => task.projectId._id === projectId
-          );
-          console.log("get to this?4");
-          setFilteredTasks(filtered);
-        }
-      } catch (err) {
-        console.error("Error loading tasks:", err);
-        setError("Failed to load tasks");
-      } finally {
+  const router =useRouter();
+  async function loadTasks() {
+    try {
+      if (!projectId && !user) {
         setLoading(false);
+        return;
       }
-    }
+      let data;
+      const userId = user?._id;
+      //check is manger
+      const role = await getUserRoleInProject(userId, projectId);
+      console.log("get to this?1", role);
 
+      if (role !== "viewer") {
+        setIsManager(true);
+        //get from db all tasks by projects
+        data = await GetTasksByProjectId(userId!, projectId);
+        console.log("get to this?2");
+        console.log("GetTasksByProjectId", data);
+        setFilteredTasks(data);
+      } else {
+        //:if !tasks
+        if (!tasks || tasks.length === 0) {
+          //get all tasks by userId and put it in store
+
+          data = await GetTasksByUserId(user?._id);
+          console.log("get to this?3");
+          console.log("GetTasksByUserId", data);
+          setTasks(data);
+        }
+        //filter tasks by projectId
+        const filtered = tasks.filter(
+          (task: any) => task.projectId._id === projectId
+        );
+        console.log("get to this?4");
+        setFilteredTasks(filtered);
+      }
+
+  
+    } catch (err) {
+      console.error("Error loading tasks:", err);
+      setError("Failed to load tasks");
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     //check existing user, projectId
-   
+
     loadTasks();
   }, [projectId, user]);
 
@@ -91,20 +95,41 @@ export default function GetProjectTasks() {
     loadTasks();
     // setFilteredTasks(updated);
   };
+
+  const fetchUsersInProject = async () => {
+    try {
+      console.log("im in fetchUsersInProject");
+      
+      const response = await getAllUsersByProjectId(projectId!);
+      console.log(response, "res");
+
+      if (response?.status !== "success") {
+        console.error("Error fetching projects:", response?.message);
+        setProjectUsers([]);
+        return;
+      }
+      setProjectUsers(response.users || []);
+    } catch (err) {
+      console.error("Error loading tasks:", err);
+      setError("Failed to load tasks");
+    }
+  };
   const onAddTask = () => {
     // Navigate to add task page
-    window.location.href = `/pages/addTask`;
-  }
+    fetchUsersInProject();
+   router.push(`/pages/addTask`);
+  };
 
   if (loading) return <p>Loading tasks...</p>;
   if (error) return <p style={{ color: "red" }}>{error}</p>;
   return (
     <div className="tasks-container">
       <h2>Project Tasks</h2>
-    {isManager ?( 
-    <button onClick={onAddTask}>Add Tasks</button> )
-    : (<p>You are a Viewer in this project.</p>)
-}
+      {isManager ? (
+        <button onClick={onAddTask}>Add Tasks</button>
+      ) : (
+        <p>You are a Viewer in this project.</p>
+      )}
       {filteredTasks.length ? (
         filteredTasks.map((task) => (
           <Task
