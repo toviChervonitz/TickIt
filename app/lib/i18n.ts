f668783c10ad604a9e468aaf9309f36a54907d9b
shiffy01 <sscheine@g.jct.ac.il>
2025-11-25 15:10:16 +0200

    added to the dictionary-- not done

--- app/lib/i18n.ts
+++ app/lib/i18n.ts
@@ -3,8 +3,8 @@ import he from "../locales/he.json";
 
 type Translations = typeof en; // ensures both en & he have the same keys
 
-export const getTranslation = (lang: "en" | "he") => (key: keyof Translations) => {
-  const t = lang === "he" ? he : en;
-  return t[key] || key;
+export const getTranslation = (lang: "en" | "he") => (key: keyof Translations): string => {
+  const t = lang === "he" ? (he as unknown as Translations) : en;
+  return (t[key] as string) || key;
 };
 
