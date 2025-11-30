67eca5e604e7e89d3ed3d4c0a58de533a1316257
shiffy01 <sscheine@g.jct.ac.il>
2025-11-30 10:07:35 +0200

    404 plus rtl

--- app/lib/i18n.ts
+++ app/lib/i18n.ts
@@ -1,12 +1,11 @@
 import { sanitizeFilter } from "mongoose";
 import en from "../locales/en.json";
 import he from "../locales/he.json";
-import ar from "../locales/ar.json";
 
 type Translations = typeof en; // ensures both en & he have the same keys
 
-export const getTranslation = (lang: "en" | "he" | "ar") => (key: keyof Translations): string => {
-  const t = lang === "he" ? (he as unknown as Translations) : lang==="en" ? (en as unknown as Translations) : (ar as unknown as Translations);
+export const getTranslation = (lang: "en" | "he") => (key: keyof Translations): string => {
+  const t = lang === "he" ? (he as unknown as Translations) : (en as unknown as Translations);
   return (t[key] as string) || key;
 };
 
