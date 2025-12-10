a2e117dd8df759dc1dddbacc8f0bdb831936c5ee
Efrat <efratdr6179@gmail.com>
2025-12-10 13:30:36 +0200

    Refactor Archive component to improve state management and update button functionality

--- app/components/Archive.tsx
+++ app/components/Archive.tsx
@@ -1,16 +1,32 @@
 import { toArchive } from "../lib/server/projectServer";
+import useAppStore from "../store/useAppStore";
 
 interface ArchiveProps {
-  projectId: string|undefined;
-  userId:string;
+  projectId: string | undefined;
+  userId: string;
+  archived: boolean;
 }
 
+export default function Archive({ projectId, userId, archived }: ArchiveProps) {
+  const { projects, setProjects } = useAppStore();
 
-export default function Archive( {projectId, userId}:ArchiveProps ) {
   async function archive(isArchive: boolean) {
     const res = await toArchive(projectId, userId, isArchive);
-    console.log("return from archive", res);
-  }
-  return <button onClick={() => archive(true)}>archive</button>;
-};
+    if (res.ok) {
+      // עדכון מקומי בסטור
+      setProjects(
+        projects.map((p) =>
+          p.project._id === projectId ? { ...p, isArchived: isArchive } : p
+        )
+      );
+    }
 
+    console.log("return from archive-------------", res);
+    console.log("state", archived);
+  }
+  return (
+    <button onClick={() => archive(!archived)}>
+      {archived ? "Restore" : "Archive"}
+    </button>
+  );
+}
